8a3cd091767a02b65201a46295a5c80a99bf3e16
ssmarkin <ssmarkin@yandex.ru>
2018-02-09 03:16:20 +0300

    update

--- PythonAnalysisData/FirstBlood/src/some.py
+++ PythonAnalysisData/FirstBlood/src/some.py
@@ -1,4 +1,7 @@
 
-sequence: list = ['Google Adwords', 'Yandex Direct', 'Facebook', 'VK', 'Partner']
-print(sequence[2:4])
+# sequence: list = ['Google Adwords', 'Yandex Direct', 'Facebook', 'VK', 'Partner', 100]
+sequence: tuple = ('Google Adwords', 'Yandex Direct', 'Facebook', 'VK', 'Partner', 100)
+
+
+print(sequence)
 
